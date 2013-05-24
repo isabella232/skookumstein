03e71ab9baa9ee52dfeafbd447adc58020410711
Hunter Loftis <hunter@hunterloftis.com>
2013-05-24 11:23:58 -0400

    updated config for demo

--- config.js
+++ config.js
@@ -10,8 +10,24 @@
   Config.prototype = {
     bundles: {
 
-      // 1
+      // 1 - Nothing on screen
+
       '49': {
+        topdown: false,
+        raycaster: false,
+        map: false,
+        npcs: false,
+        segments: 1,
+        intersections: false,
+        columns: 'flat',
+        textures: false,
+        shadows: false,
+        collisions: false
+      },
+
+      // 2 - Player on screen
+
+      '50': {
         topdown: true,
         raycaster: false,
         map: false,
@@ -24,8 +40,9 @@
         collisions: false
       },
 
-      // 2
-      '50': {
+      // 3 - Map on screen
+
+      '51': {
         topdown: true,
         raycaster: false,
         map: true,
@@ -35,11 +52,27 @@
         columns: 'flat',
         textures: false,
         shadows: false,
+        collisions: false
+      },
+
+      // 4 - Intersections
+
+      '52': {
+        topdown: true,
+        raycaster: false,
+        map: true,
+        npcs: false,
+        segments: 1,
+        intersections: true,
+        columns: 'flat',
+        textures: false,
+        shadows: false,
         collisions: true
       },
 
-      // 3
-      '51': {
+      // 5 - Field of View
+
+      '53': {
         topdown: true,
         raycaster: false,
         map: true,
@@ -52,8 +85,9 @@
         collisions: true
       },
 
-      // 4
-      '52': {
+      // 6 - Walls (3d, flat, blocky)
+
+      '54': {
         topdown: true,
         raycaster: true,
         map: true,
@@ -66,25 +100,12 @@
         collisions: true
       },
 
-      // 5
-      '53': {
-        topdown: true,
-        raycaster: true,
-        map: true,
-        npcs: false,
-        segments: 128,
-        intersections: true,
-        columns: 'flat',
-        textures: false,
-        shadows: false,
-        collisions: true
-      },
+      // 7 - Walls (3d, flat, smooth)
 
-      // 6
-      '54': {
+      '55': {
         topdown: false,
         raycaster: true,
-        map: true,
+        map: false,
         npcs: false,
         segments: 128,
         intersections: true,
@@ -94,8 +115,9 @@
         collisions: true
       },
 
-      // 7
-      '55': {
+      // 8 - Lights
+
+      '56': {
         topdown: false,
         raycaster: true,
         map: false,
@@ -108,21 +130,8 @@
         collisions: true
       },
 
-      // 8
-      '56': {
-        topdown: false,
-        raycaster: true,
-        map: false,
-        npcs: false,
-        segments: 128,
-        intersections: true,
-        columns: 'textured',
-        textures: true,
-        shadows: false,
-        collisions: true
-      },
+      // 9 - Textures
 
-      // 9
       '57': {
         topdown: false,
         raycaster: true,
@@ -131,18 +140,19 @@
         segments: 128,
         intersections: true,
         columns: 'textured',
-        textures: false,
+        textures: true,
         shadows: true,
         collisions: true
       },
 
-      // Q
+      // Q - Low resolution
+      //
       '113': {
         topdown: false,
         raycaster: true,
         map: false,
         npcs: false,
-        segments: 256,
+        segments: 32,
         intersections: true,
         columns: 'textured',
         textures: true,
@@ -150,13 +160,14 @@
         collisions: true
       },
 
-      // W
+      // W - High resolution
+
       '119': {
         topdown: false,
         raycaster: true,
         map: false,
         npcs: false,
-        segments: 128,
+        segments: 256,
         intersections: true,
         columns: 'textured',
         textures: true,
